4eb660d2d82a0e148ce6b1f311a89a66e2c223c5
Abhinandan-Sah <abhinandansah222@gmail.com>
2024-01-16 20:02:19 +0530

    updated

--- EJS Dir(Templeting)/index.js
+++ EJS Dir(Templeting)/index.js
@@ -14,9 +14,15 @@ app.get("/", (req, res) =>  {
 app.get("/home", (req, res) =>  {
     res.send("hello")
 });
-app.get("/roll", (req, res) =>  {
-    res.send("rolldice.ejs");
+app.get("/rolldice", (req, res) =>  {
+    let diceVal = Math.floor(Math.random() * 6) + 1;
+    res.render("rolldice.ejs", {diceVal : diceVal});
 });
+
+app.get("/ig/:username", (req, res) => {
+    let {username} = req.params;
+    res.render("instagram.ejs", {username} );
+})
 
 app.listen(port, () => {
     console.log(`listening on Port ${port}`);
