801c9cfc22ddf236bba9dea1f411b44ae63fc211
Abhinandan-Sah <abhinandansah222@gmail.com>
2024-02-14 22:39:09 +0530

    updated

--- 13-MongoDB/index.js
+++ 13-MongoDB/index.js
@@ -18,12 +18,38 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
-const user1 = new User({
-    name: "Adam",
-    email: "adam@gamil.com",
-    age: 48,
+User.find({age: {$gt: 47}})
+.then((res) =>{
+    console.log(res[0].name);
+})
+.catch((err) => {
+    console.log(err);
 });
 
-user1.save();
+// User.insertMany([
+//     {name: "Tony", email: "tony@gmail.com", age: 50},
+//     {name: "peter", email: "peter@gmail.com", age: 30},
+//     {name: "Bruce", email: "bruce@gmail.com", age: 47},
+// ]).then((res) => {
+//     console.log(res);
+// })
+
+/* This are insert as one */
+// const user2 = new User({
+//     name: "Eve",
+//     email: "eve@gamil.com",
+//     age: 48,
+// });
+
+// user2
+//     .save()
+//     .then((res) =>{
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+// });
+
+// user1.save();
 
 // mongoose.connect('mongodb://127.0.0.1:27017/test');
