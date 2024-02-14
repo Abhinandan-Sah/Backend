f69bd0a95e091a09f52340025e962132f6e643f5
Abhinandan-Sah <abhinandansah222@gmail.com>
2024-02-14 19:57:29 +0530

    updated

--- 13-MongoDB/index.js
+++ 13-MongoDB/index.js
@@ -10,4 +10,20 @@ async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/test');
 }
 
+const userSchema = new mongoose.Schema({
+    name: String, 
+    email: String, 
+    age: Number,
+});
+
+const User = mongoose.model("User", userSchema);
+
+const user1 = new User({
+    name: "Adam",
+    email: "adam@gamil.com",
+    age: 48,
+});
+
+user1.save();
+
 // mongoose.connect('mongodb://127.0.0.1:27017/test');
