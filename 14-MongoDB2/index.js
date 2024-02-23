6451d99d4e2d4811497356f0d6425e0744ad9af1
Abhinandan-Sah <abhinandansah222@gmail.com>
2024-02-24 01:18:27 +0530

    up

--- 14-MongoDB2/index.js
+++ 14-MongoDB2/index.js
@@ -3,10 +3,13 @@ const { default: mongoose } = require("mongoose");
 const app = express();
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 main()
     .then(() => {
@@ -31,10 +34,69 @@ async function main(){
 //     console.log(res);
 // })
 
+//create Route
+app.post("/chats", (req, res)=>{
+    let {from, msg, to }= req.body;
+    let newChat= new Chat({
+        from: from,
+        to: to,
+        msg: msg,
+        created_at: new Date()
+    })
+    newChat
+    .save().then((res) =>{
+        console.log("chat was saved");
+    })
+    .catch((err) =>{
+        console.log(err);
+    })
+    
+    res.redirect("/chats");
+});
+
+//Destroy Route
+app.delete("/chats/:id", async (req, res) =>{
+    let {id} = req.params;
+    let deletedChat  = await Chat.findByIdAndDelete(id);
+    console.log(deletedChat);
+    res.redirect("/chats");
+})
+
+//Update Route
+app.put("/chats/:id", async (req, res) => {
+    let {id}= req.params;
+    let {msg: newMsg} = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate(
+        id,
+        {msg: newMsg},
+        {runValidators: true, new: true} 
+    )
+    console.log(updatedChat);
+    res.redirect("/chats");
+})
+
+// Edit route
+app.get("/chats/:id/edit", async (req, res) => {
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", {chat});
+});
+
+// New Route
+app.get("/chats/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+// Index Route
 app.get("/chats", async (req, res) =>{
     let chats = await Chat.find();
-    console.log(chats);
+    // console.log(chats);
     res.render("index.ejs", {chats});
+})
+
+//New Route
+app.get("/chats/new", (req, res) =>{
+    res.render("new.ejs");
 })
 
 app.get("/", (req, res) =>{
